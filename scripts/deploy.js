07ccd90d477f0b5347a703a4605d7997b1af90d0
Igor Yalovoy <to@ylv.io>
2021-11-03 14:44:01 +0000

    Deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,20 +3,27 @@ const { unit } = require('../lib/helpers');
 async function main() {
   [deployer, owner] = await ethers.getSigners();
 
-  const erc20Factory = await ethers.getContractFactory('ERC20Mock');
-  const bablToken = await erc20Factory.deploy('Babylon Finance', 'BABL', owner.address, unit(1000000));
-  await bablToken.deployTransaction.wait( 6);
-  console.log(`BABL ${bablToken.address}`);
+  const { chainId } = await ethers.provider.getNetwork();
+  console.log('chainId', chainId);
 
-  await hre.run('verify:verify', {
-    address: bablToken.address,
-    constructorArguments: ['Babylon Finance', 'BABL', owner.address, unit(1000000)],
-  });
+  const erc20Factory = await ethers.getContractFactory('ERC20Mock');
+  let bablToken;
+  // Rinkeby
+  if (chainId === 4) {
+    bablToken = await erc20Factory.deploy('Babylon Finance', 'BABL', owner.address, unit(1000000));
+    await bablToken.deployTransaction.wait(6);
+    console.log(`BABL ${bablToken.address}`);
+
+    await hre.run('verify:verify', {
+      address: bablToken.address,
+      constructorArguments: ['Babylon Finance', 'BABL', owner.address, unit(1000000)],
+    });
+  }
 
   const prophetsFactory = await ethers.getContractFactory('Prophets');
 
   const nft = await prophetsFactory.deploy(bablToken.address);
-  await nft.deployTransaction.wait( 6);
+  await nft.deployTransaction.wait(6);
   console.log(`NFT ${nft.address}`);
 
   await hre.run('verify:verify', {
@@ -27,25 +34,31 @@ async function main() {
   await nft.transferOwnership(owner.address);
   await bablToken.connect(owner).transfer(nft.address, unit(40000));
 
-  const wethToken = await erc20Factory.deploy('Wrapped ETH', 'WETH', owner.address, unit(1e10));
-  await wethToken.deployTransaction.wait( 6);
-  console.log(`WETH ${wethToken.address}`);
+  let wethToken;
+  // Rinkeby
+  if (chainId === 4) {
+    wethToken = await erc20Factory.deploy('Wrapped ETH', 'WETH', owner.address, unit(1e10));
+    await wethToken.deployTransaction.wait(6);
+    console.log(`WETH ${wethToken.address}`);
 
-  await hre.run('verify:verify', {
-    address: wethToken.address,
-    constructorArguments: ['Wrapped ETH', 'WETH', owner.address, unit(1e10)],
-  });
+    await hre.run('verify:verify', {
+      address: wethToken.address,
+      constructorArguments: ['Wrapped ETH', 'WETH', owner.address, unit(1e10)],
+    });
+  }
 
   const arrivalFactory = await ethers.getContractFactory('ProphetsArrival');
-  const arrival = await arrivalFactory.deploy(nft.address, wethToken.address);
-  await arrival.deployTransaction.wait( 6);
+  const block = await ethers.provider.getBlock();
+  const arrivalStart = chainId === 1 ? 1636992000 : block.timestamp + 60;
+  const arrival = await arrivalFactory.deploy(nft.address, wethToken.address, arrivalStart);
+  await arrival.deployTransaction.wait(6);
   console.log(`Arrival ${arrival.address}`);
 
   await arrival.transferOwnership(owner.address);
 
   await hre.run('verify:verify', {
     address: arrival.address,
-    constructorArguments: [nft.address, wethToken.address],
+    constructorArguments: [nft.address, wethToken.address, arrivalStart],
   });
 
   await nft.connect(owner).setMinter(arrival.address);
