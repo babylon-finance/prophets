6db134e69e2846b34e25c271ae02dd7d522c1128
Igor Yalovoy <to@ylv.io>
2021-11-11 01:55:17 +0000

    Make canMint pubcli

--- contracts/ProphetsArrival.sol
+++ contracts/ProphetsArrival.sol
@@ -173,19 +173,20 @@ contract ProphetsArrival is Initializable, OwnableUpgradeable, ReentrancyGuardUp
         return (attr.bablLoot * DENOM_FLOOR_PRICE_BABL) / 1e18;
     }
 
-    /* ============ Internal Write Functions ============ */
-
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
-    /* ============ Internal View Functions ============ */
-
-    function canMintProphet(address _user, bytes32[] calldata _proof) private view returns (bool) {
+    function canMintProphet(address _user, bytes32[] calldata _proof) public view returns (bool) {
         bool isFirst = MerkleProof.verify(_proof, firstRoot, _leaf(_user));
         return
             isThirdRound() ||
             (isFirstRound() && isFirst) ||
             (isSecondRound() && (isFirst || MerkleProof.verify(_proof, secondRoot, _leaf(_user))));
     }
+
+    /* ============ Internal Write Functions ============ */
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /* ============ Internal View Functions ============ */
+
 
     function isFirstRound() private view returns (bool) {
         return block.timestamp >= eventStartsTS && block.timestamp < secondRoundTS;
