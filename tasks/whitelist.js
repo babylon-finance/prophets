d4d63b73715d27bc6a9076424fb3087cace01fd3
Igor Yalovoy <to@ylv.io>
2021-11-14 02:03:47 +0300

    Fix whitelist script

--- tasks/whitelist.js
+++ tasks/whitelist.js
@@ -3,6 +3,8 @@ const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 const { task } = require('hardhat/config');
 
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
+
 const {
   getMerkleTree,
   unit,
@@ -14,15 +16,15 @@ const {
   ZERO_ADDRESS,
   HASH_ZERO,
   getPrices,
-  BlockNativePriceProvider
+  BlockNativePriceProvider,
 } = require('../lib/helpers');
 
 function getCount(path) {
-  return JSON.parse(fs.readFileSync(path)).map((s) => s.address).length;
+  return JSON.parse(fs.readFileSync(path)).length;
 }
 
 function getRootFromJSON(path) {
-  return getMerkleTree(JSON.parse(fs.readFileSync(path)).map((s) => s.address)).getHexRoot();
+  return getMerkleTree(JSON.parse(fs.readFileSync(path))).getHexRoot();
 }
 
 //npx hardhat whitelist --settlers ./settlers.json --firsts ./firsts.json  --seconds ./seconds.json
@@ -36,6 +38,8 @@ task('whitelist')
     const { chainId } = await ethers.provider.getNetwork();
     console.log('chainId', chainId);
 
+    const url = `https://${chainId === 1 ? 'eth-mainnet' : 'eth-rinkeby'}.alchemyapi.io/v2/${ALCHEMY_KEY}`;
+
     const owner =
       chainId !== 31337
         ? new ethers.Wallet(`0x${process.env.OWNER_PRIVATE_KEY}`, new BlockNativePriceProvider(url))
@@ -43,9 +47,9 @@ task('whitelist')
 
     const arrivalContract = await ethers.getContractAt('ProphetsArrival', arrival);
     console.log(
-      `Updating whitelist with settlers (${getCount(settlers)}), firsts (${getCount(
-        settlers,
-      )}), and seconds (${getCount(settlers)}) `,
+      `Updating whitelist with settlers (${getCount(settlers)}), first round (${getCount(
+        firsts,
+      )}), and second round(${getCount(seconds)}) `,
     );
     const tx = await arrivalContract
       .connect(owner)
