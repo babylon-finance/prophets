da6c869f386e17aa5e0f1eabf7d9ab4fbebee732
rriescog <29550529+rriescog@users.noreply.github.com>
2021-11-18 20:37:23 +0100

    getStakedAndAttributes (#5)
    * getStakedAndAttributes
    
    * updating function name
    
    * Optimize gas
    
    * Prettify code
    
    Co-authored-by: Igor Yalovoy <to@ylv.io>

--- test/ProphetsNFT.test.js
+++ test/ProphetsNFT.test.js
@@ -245,9 +245,7 @@ describe('ProphetsNFT', () => {
 
     it('can claim loot only if not claimed before', async function () {
       await nft.connect(ramon).batchClaimLoot([1, 2, 3, 4, 5]);
-      await expect(nft.connect(ramon).batchClaimLoot([1, 2, 3, 4, 5])).to.be.revertedWith(
-        'Loot already claimed',
-      );
+      await expect(nft.connect(ramon).batchClaimLoot([1, 2, 3, 4, 5])).to.be.revertedWith('Loot already claimed');
     });
 
     it('can claim loot only for own prophets', async function () {
@@ -363,6 +361,48 @@ describe('ProphetsNFT', () => {
       expect(lp).to.eq(from(100));
       expect(voter).to.eq(0);
       expect(strategist).to.eq(0);
+    });
+  });
+
+  describe('getStakedProphetAttrs', function () {
+    it('can get prophets attributes of a given staked NFT', async function () {
+      await nft.connect(minter).mintProphet(ramon.address);
+      await nft.connect(ramon).stake(1, ramon.address);
+      expect(await nft.stakeOf(ramon.address, ramon.address)).to.eq(1);
+      const [id, babl, strategist, voter, lp, creator] = await nft.getStakedProphetAttrs(ramon.address, ramon.address);
+      expect(id).to.eq(1);
+      expect(babl).to.eq(unit(5));
+      expect(strategist).to.eq(0);
+      expect(voter).to.eq(0);
+      expect(lp).to.eq(unit(0.01));
+      expect(creator).to.eq(0);
+    });
+    it('can get great prophets attributes of a given staked great NFT', async function () {
+      // creator, lp, voter, strategist
+      await nft
+        .connect(owner)
+        .setProphetsAttributes([8001], [unit(8)], [from(50)], [from(100)], [from(300)], [from(400)]);
+      await nft.connect(minter).mintGreatProphet(ramon.address, 8001);
+      await nft.connect(ramon).stake(8001, ramon.address);
+      expect(await nft.stakeOf(ramon.address, ramon.address)).to.eq(8001);
+      const [id, babl, strategist, voter, lp, creator] = await nft.getStakedProphetAttrs(ramon.address, ramon.address);
+      expect(id).to.eq(8001);
+      expect(babl).to.eq(unit(8));
+      expect(strategist).to.eq(unit(0.04));
+      expect(voter).to.eq(unit(0.03));
+      expect(lp).to.eq(unit(0.01));
+      expect(creator).to.eq(unit(0.005));
+    });
+    it('can NOT get prophets attributes (equal zero) if NFT is not staked yet', async function () {
+      await nft.connect(minter).mintProphet(ramon.address);
+      expect(await nft.stakeOf(ramon.address, ramon.address)).to.eq(0);
+      const [id, babl, strategist, voter, lp, creator] = await nft.getStakedProphetAttrs(ramon.address, ramon.address);
+      expect(id).to.eq(0);
+      expect(babl).to.eq(0);
+      expect(strategist).to.eq(0);
+      expect(voter).to.eq(0);
+      expect(lp).to.eq(0);
+      expect(creator).to.eq(0);
     });
   });
 
